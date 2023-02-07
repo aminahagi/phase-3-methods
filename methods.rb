c854184456df9f040e7923ad38e6669384f4b112
aminahagi <amina.hagi@student.moringaschool.com>
2023-02-07 11:52:55 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,13 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet_with_default "programmer"
