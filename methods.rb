c96ed30dea6294a4f9fa29951af64e99a6b8580a
aminahagi <amina.hagi@student.moringaschool.com>
2023-02-07 12:03:29 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -10,4 +10,16 @@ end
 def greet_with_default(name = "Naureen")
     puts "Hello, #{name}!"
 end
-greet_with_default "programmer"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+     num1 + num2
+end
+
+def halve(num)
+    return nil unless num.is_a?(Integer)
+    num / 2
+end
